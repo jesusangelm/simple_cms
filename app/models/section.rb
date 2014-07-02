9c5d862bda46b9fdcee8b0bde5451b4a2cfa0912
Jesus Marin <jesusangelm@gmail.com>
2014-07-02 11:07:04 -0430

    add some callbacks

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,14 +3,17 @@ class Section < ActiveRecord::Base
   belongs_to :page
   has_many :section_edits
   has_many :editors, :through => :section_edits,
-           :class_name => "AdminUser"
+    :class_name => "AdminUser"
 
   acts_as_list :scope => :page
+
+  #callback
+  after_save :touch_page
 
   CONTENT_TYPE = ["text", "HTML"]
 
   validates :name, presence: true,
-                   length: { maximum: 255 }
+    length: { maximum: 255 }
   #validates :content_type, in: CONTENT_TYPE,
   #                         message: "must be one of: #{CONTENT_TYPE.join(", ")}"
   validates :content, presence: true
@@ -20,4 +23,11 @@ class Section < ActiveRecord::Base
   scope :sorted, lambda { order("sections.position ASC") }
   scope :newest_first, lambda { order("sections.created_at DESC")}
 
+  private
+
+  def touch_page
+    # touch es similar a:
+    # subject.update_attribute(:updated_at, Time.now)
+    page.touch
+  end
 end
