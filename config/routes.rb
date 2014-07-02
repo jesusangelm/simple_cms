d6f2dd815916227e3fafe0b056c7cd2c150c3abf
Jesus Marin <jesusangelm@gmail.com>
2014-07-02 11:40:42 -0430

    add public controller for a public page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  root 'public#index'
+
+  get "show/:permalink", :to => "public#show"
+
   get "admin", to: 'access#index'
 
   match ":controller(/:action(/:id))", via: [ :get, :post ]
